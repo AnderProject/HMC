04f01a7a0449c10f2240d893cb9bce61bc339a9f
Batman <merchanbalcazaranderson@gmail.com>
2025-04-07 02:51:38 -0500

    up12

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: waitress-serve --listen=0.0.0.0:$PORT hotel_system.wsgi:application
--- hotel_system/wsgi.py
+++ hotel_system/wsgi.py
@@ -10,13 +10,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotel_system.settings')
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotel_system.settings')
-
-BASE_DIR = Path(__file__).resolve().parent.parent  # 👈 Define BASE_DIR correctamente
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))  # 👈 Esto ya no da error
